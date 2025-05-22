7825831304af77f3cb759b329b0719655d9da5a7
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-05-22 13:54:29 -0300

    Create Card.cs
    Criação da entity Card

---
+++ Domain/Entity/Card.cs
@@ -0,0 +1,11 @@
+namespace Domain.Entity;
+
+public class Card{
+    public Guid Id { get; set; }
+    public string? Titlu { get; set; }
+    public string? Description { get; set; }
+    public DateTime? CreateAt { get; set; } = DateTime.Now;
+    public DateTime? Daedline { get; set; }
+    public ListCard? Lis { get; set; }
+    public StatusItemEnum Status { get; set; }
+}
