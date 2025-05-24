24e96671f3e06e6ef514411ff33cab7a73f67057
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-05-23 21:44:40 -0300

    Add Pacotes
    Infra
    Microsoft.EntityFrameworkCore
    Microsoft.EntityFrameworkCore.SqlServer
    Microsoft.EntityFrameworkCore.Tools
    
    MinhaApi
    
    Microsoft.EntityFrameworkCore
    Microsoft.EntityFrameworkCore.Design

--- MinhaApi/obj/Debug/net9.0/MinhaApi.AssemblyInfo.cs
+++ MinhaApi/obj/Debug/net9.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3f1b44d97ff67900a5a2879d09fd5fafcad5a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d54c051e102673ea06b157ce94fcbfcbb74bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
